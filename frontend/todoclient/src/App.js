762a06cc65e0c8b0b40384c1eba5060cfe296912
Cheery <r.mohammadifarsani@gmail.com>
2024-09-27 12:54:27 +0200

    config and test tailwindcss

--- frontend/todoclient/src/App.js
+++ frontend/todoclient/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="text-white bg-blue-500 border-blue-600 border-b p-4 rounded-sm">
+      <span className="font-bold text-3xl ml-4">Welcome</span>
+    </div>
+  );
 }
 
 export default App;
