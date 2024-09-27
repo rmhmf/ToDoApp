5948f9b9496e4ff539d1f21d0e21ccc34d4642c9
Cheery <r.mohammadifarsani@gmail.com>
2024-09-27 18:24:40 +0200

    improivng style of header and content

--- frontend/todoclient/src/component/Content/Content.jsx
+++ frontend/todoclient/src/component/Content/Content.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import sticks from "../../assets/sticks.png";
 
 function Content() {
   return (
-    <div className="flex flex-grow">
+    <div className="flex flex-grow bg-amber-100">
       <div className="flex justify-center items-center w-1/2">
         <div className="flex-inline flex-col space-y-4 font-roboto">
-          <h2 className="font-bold text-3xl">Organize yourself efficiently</h2>
+          <h2 className="font-bold text-4xl">Organize yourself efficiently</h2>
           <p>
             Hold your notes, plans, and ideas in your pocket.
             <br /> Enjoy modern design and much more ...
@@ -15,11 +16,7 @@ function Content() {
         </div>
       </div>
       <div className="flex justify-left items-center w-1/2">
-        <img
-          className="w-4/5"
-          src="https://s3-alpha.figma.com/hub/file/4512334802/659e8d64-4c73-4e3e-b07c-5e8e3d76f180-cover.png"
-          alt="image of sticks"
-        />
+        <img className="w-4/5" src={sticks} alt="sticks" />
       </div>
     </div>
   );
--- frontend/todoclient/src/component/Header/Header.jsx
+++ frontend/todoclient/src/component/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import EventNoteIcon from "@mui/icons-material/EventNote";
 
 export default function Header() {
   return (
-    <div className="flex justify-between text-white bg-blue-500 border-blue-600 border-b p-4 rounded-sm">
-      <h1 className="font-roboto font-bold text-3xl ml-4">Welcome</h1>
-      <div className="flex space-x-2 mr-4">
+    <div className="flex h-20 justify-between bg-amber-300 border-amber-400 border-b p-4 rounded-sm shadow-sm">
+      <div className="flex items-center space-x-2 ml-4">
+        <EventNoteIcon />
+        <p className="font-roboto text-gray-500 font-bold text-3xl">
+          Sticky Note
+        </p>
+      </div>
+
+      <div className="flex items-center space-x-2 mr-4">
         <Button variant="outlined">Login</Button>
         <Button variant="contained">Sign in</Button>
       </div>
