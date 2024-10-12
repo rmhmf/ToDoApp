30669fca93a7df8718ba13f02af01cf7d9b6989f
Cheery <r.mohammadifarsani@gmail.com>
2024-10-13 00:49:28 +0200

    add fullcalendar with basic functionality

--- frontend/src/component/CalendarPage/CalendarPage.jsx
+++ frontend/src/component/CalendarPage/CalendarPage.jsx
@@ -1,16 +1,50 @@
-import React from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
+import { React, useRef } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { Button, IconButton, Toolbar } from "@mui/material";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 
 function CalendarPage(props) {
-  const localizer = momentLocalizer(moment);
+  const calendarRef = useRef(null);
+
+  function renderEvent(event) {
+    return <p className="bg-blue-100 p-1">{event.event.title}</p>;
+  }
+
+  function next() {
+    const calendarApi = calendarRef.current.getApi();
+    calendarApi.next();
+  }
+
+  function prev() {
+    const calendarApi = calendarRef.current.getApi();
+    calendarApi.prev();
+  }
+
+  const events = [{ title: "sleep Early", date: "2024-10-12" }];
+
   return (
-    <div className="">
-      <Calendar
-        style={{ height: 500 }}
-        localizer={localizer}
-        startAccessor="start"
-        endAccessor="end"
+    <div className="p-8 w-full">
+      <Toolbar>
+        <IconButton onClick={prev}>
+          <NavigateBeforeIcon />
+        </IconButton>
+        <IconButton onClick={next}>
+          <NavigateNextIcon />
+        </IconButton>
+      </Toolbar>
+      <FullCalendar
+        ref={calendarRef}
+        selectable={true}
+        editable={true}
+        headerToolbar={false}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        events={events}
+        eventContent={renderEvent}
       />
     </div>
   );
